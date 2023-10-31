5731f14c2633832d290700761e6938480af8ec1e
Joacim Vetterlain <joacim@vetterlain.dk>
2023-10-31 22:33:39 +0100

    created camera

--- src/Scenes/game.ts
+++ src/Scenes/game.ts
@@ -1,37 +1,51 @@
-import Phaser from 'phaser'
-
+import { Scene } from 'phaser'
 import { Boat } from './boats/boat-port'
-import { WoodenBoat } from './boats/wooden-boat';
 import { ControlPort } from './controls/control-port';
-import { KeyboardController } from './controls/keyboard-controller';
+import { CameraPort } from './camera/camera-port';
+import { WoodenBoat } from './boats/wooden-boat';
 
+export interface GameplayProps {
+  controlPort: ControlPort;
+  cameraPort: CameraPort;
+  scene: Scene;
+}
 
-export default class Gameplay extends Phaser.Scene {
-  private controlPort: ControlPort = {} as ControlPort
+export default class Gameplay {
   private boat: Boat = {} as Boat;
+  private cameraPort: CameraPort;
+  private controlPort: ControlPort;
+  public scene: Scene;
 
-  preload() {
-    this.load.image('boat', './boat.png')
+
+  public constructor(props: GameplayProps) {
+    this.cameraPort = props.cameraPort;
+    this.controlPort = props.controlPort;
+    this.scene = props.scene;
   }
 
+  
   create() {
-    this.controlPort = new KeyboardController();
-    // Set background color to dark blue
-    this.cameras.main.setBackgroundColor(0x000066);
-    this.boat = new WoodenBoat({ app: this, controlPort: this.controlPort });
+    this.boat = new WoodenBoat({ controlPort: this.controlPort, scene: this.scene });
+    this.cameraPort.setFollow(this.boat.sprite);
+    // remove the default camera
+    this.scene.cameras.remove(this.scene.cameras.main);
+    this.scene.cameras.addExisting(this.cameraPort.camera);
     // Create a sprite at the center of the screen using the 'logo' image.
-    const island = this.physics.add.sprite(this.cameras.main.centerX + 100, this.cameras.main.centerY + 100, 'boast');
+    const island = this.scene.physics.add.sprite(this.scene.cameras.main.centerX + 100, this.scene.cameras.main.centerY + 100, 'boast');
     // Make island static
     island.setImmovable(true);
     // Draw a rectangle around the island
-    const graphics = this.add.graphics();
+    const graphics = this.scene.add.graphics();
     graphics.lineStyle(1, 0xffffff);
     graphics.strokeRect(island.x - island.width / 2, island.y - island.height / 2, island.width, island.height);
     // Add collision detection between boat and island
-    this.physics.add.collider(this.boat.sprite, island);
+    // this.physics.add.collider(this.boat.sprite, island);
   }
 
   update(time: number, delta: number) {
     this.boat.update(delta);
+    this.cameraPort.update(delta);
+    // UI
+
   }
 }
---
+++ src/test/camera.test.ts
@@ -0,0 +1,28 @@
+// import { CameraPort } from '../Scenes/camera/camera-port';
+// import { mock } from 'jest-mock-extended';
+// import Gameplay from '../Scenes/game';
+// import { ControlPort } from '../Scenes/controls/control-port';
+// import  { Scene } from 'phaser';
+// const Phaser = require("phaser");
+describe('Camera', () => {
+    it('should be tested', () => {
+        
+        // const cameraPort = mock<CameraPort>();
+        // const controlPort = mock<ControlPort>();
+        // const phaser = mock<Phaser>();
+        // const scene = new Scene({ visible: true });
+        // const game = new Gameplay({ cameraPort, controlPort, scene });
+
+        // let games = new Phaser.Game({ type: Phaser.HEADLESS });
+
+        // game.create();
+        // game.update(0, 0);
+
+        // expect(game).toBeDefined();
+        // expect(cameraPort.setFollow).toHaveBeenCalled();
+        // expect(cameraPort.update).toHaveBeenCalled();
+
+    });
+});
+
+
