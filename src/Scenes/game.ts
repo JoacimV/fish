7f2d15b38e51c3881e86e3ca305862fb02e7b8af
Joacim Vetterlain <joacim@vetterlain.dk>
2024-07-18 21:23:17 +0200

    don't react to cursor

--- src/Scenes/game.ts
+++ src/Scenes/game.ts
@@ -59,24 +59,25 @@ export default class Gameplay {
     // Add a customer cursor
     this.scene.input.setDefaultCursor('url(bobber.png), pointer');
     // Get cursor position
-    const cursor = this.scene.input.activePointer;
+    // const cursor = this.scene.input.activePointer;
     // log what type of tile the cursor is over
     // if cursor is clicked
-    if (cursor.isDown) {
-      // Draw a image at the cursor position
-      const boatImage = this.scene.add.sprite(cursor.worldX, cursor.worldY, 'animatedBobber');
-      // Cycle through the animation
-      this.scene.anims.create({
-        key: 'bobber',
-        frames: this.scene.anims.generateFrameNumbers('animatedBobber', { start: 0, end: 3 }),
-        frameRate: 2,
-        repeat: -1
-      });
-      boatImage.anims.play('bobber', true);
-      // Connect a line between the image and the boat      
-      const line = this.scene.add.graphics();
-      line.lineStyle(1, 0xffffff, 1);
-      line.lineBetween(boatImage.x, boatImage.y, this.boat.sprite.x, this.boat.sprite.y);
-    }
+    // if (cursor.isDown) {
+
+    // // Draw a image at the cursor position
+    // const boatImage = this.scene.add.sprite(cursor.worldX, cursor.worldY, 'animatedBobber');
+    // // Cycle through the animation
+    // this.scene.anims.create({
+    //   key: 'bobber',
+    //   frames: this.scene.anims.generateFrameNumbers('animatedBobber', { start: 0, end: 3 }),
+    //   frameRate: 2,
+    //   repeat: -1
+    // });
+    // boatImage.anims.play('bobber', true);
+    // // Connect a line between the image and the boat      
+    // const line = this.scene.add.graphics();
+    // line.lineStyle(1, 0xffffff, 1);
+    // line.lineBetween(boatImage.x, boatImage.y, this.boat.sprite.x, this.boat.sprite.y);
+    // }
   }
 }
